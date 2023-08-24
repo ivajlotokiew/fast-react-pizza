89521cee218bd332ebcc8fd34a17581b03cd1117
ivajlotokiew <ivajlo_nick@abv.bg>
2023-08-24 16:39:49 +0300

    Fetching data with react router

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,22 @@
-function Menu() {
-  return <h1>Menu</h1>;
-}
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
+const Menu = () => {
+  const menu = useLoaderData();
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
+};
+
+export const loader = async () => {
+  const menu = await getMenu();
+
+  return menu;
+};
 
 export default Menu;
